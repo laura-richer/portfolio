18501d98648d4e7e2673a4e68db7f13046981341
laura-richer <lauraricher@gmail.com>
2024-06-11 20:52:02 +0100

    chore: adding sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [sitemap()],
+  site: 'https://www.lauraricher.co.uk',
   compressHTML: true,
   markdown: {
     shikiConfig: {
